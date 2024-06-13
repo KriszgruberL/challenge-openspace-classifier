487bf8b0ceb8aef1d6db98cc5d7e4016750a2f45
.com <atome@kali.Atome>
2024-06-13 15:28:04 +0200

    main start

--- Utils/OpenSpace.py
+++ Utils/OpenSpace.py
@@ -26,32 +26,43 @@ class Openspace:
         """
         return self.nbCapacity
     
-    def organised(self, listName: List[People|str]) -> None:
+    def organised(self, listName: List[List[People|str]]) -> None:
         """
         Organizes people into tables in the openspace.
 
         Args:
-            listName (List[People | str]): List of people names or empty strings for empty seats.
+            listName (List[List[People|str]]): List of people names or empty strings for empty seats.
         """
         table = Table()
         countBlank = 0
 
         # Shuffle the list of names
         random.shuffle(listName)
-        
+
         # Add all people to a table
         for i in listName:
+            # Check if the openspace has reached its maximum capacity
+            if len(self.openspace) * Table.getCapacity() + len(table) >= self.nbCapacity:
+                break
+
             # Check if the table is full
             if table.getLeftCapacity - countBlank <= 0:
                 self.addOpenspace(table)
                 table = Table()
-
                 countBlank = 0
 
             if i != '':
-                self.openspace.assignSeat(i)
+                table.assignSeat(i)
             else:
                 countBlank += 1
+
+        # Add the last table if it's not empty
+        if table:
+            self.addOpenspace(table)
+
+        remaining_people = listName[self.nbCapacity:]
+        if remaining_people:
+            print("These people could not be seated due to capacity limits:", remaining_people)
 
     def addOpenspace(self, table: Table) -> None:
         """
@@ -60,8 +71,8 @@ class Openspace:
         Args:
             table (Table): The table to add to the openspace.
         """
-        
         self.openspace.append(table)
+
 
     def store(self, filename: str) -> None:
         """
