6667f9f0ab81787b79b034273cf15560f5c3a426
LKriszgruber <l.kriszgruber@gmail.com>
2024-06-13 16:22:57 +0200

    .

--- Test/TestOpenspace.py
+++ Test/TestOpenspace.py
@@ -4,15 +4,15 @@ import unittest
 
 class TestOpenspace:
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../Utils')))
-    # def getNbCapacity(self) -> int:
-    #
-    #     return self.nbCapacity
-    #
-    # def organised(self, listName: List[List[People | str]]) -> None:
-    #
-    #
-    # def addOpenspace(self, table: Table) -> None:
-    #
-    #
-    # def store(self, filename: str) -> None:
+    def testGetNbCapacity(self):
+
+        return self.nbCapacity
+
+    def organised(self, listName: List[List[People | str]]) -> None:
+
+
+    def addOpenspace(self, table: Table) -> None:
+
+
+    def store(self, filename: str) -> None:
 
