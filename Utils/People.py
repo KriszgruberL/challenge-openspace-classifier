2921a6816230ce19ac45376ba54db63944bcf54a
.com <atome@kali.Atome>
2024-06-13 11:43:39 +0200

    creation de la class People.py et des method

---
+++ Utils/People.py
@@ -0,0 +1,23 @@
+class People:
+    """
+        This class is the representation of a Person
+    """
+
+    def __init__(self, name:str):
+        """
+        Contructor of the class People.py
+
+        Parameters
+        ----------
+    
+        name : str
+            name of person
+
+        """
+        self.name = name # the name of the People
+
+    def getName(self):
+        """
+            Return the name of the People.py
+        """
+        return self.name
